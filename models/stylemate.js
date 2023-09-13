f0bc9fdf7bca782b9d1a9198d348eb18a730d3a0
hirostory <hiroakisotooka@gmail.com>
2023-09-13 18:24:24 -0400

    change the model

--- models/stylemate.js
+++ models/stylemate.js
@@ -1,9 +1,33 @@
 const mongoose = require('mongoose')
 
 const itemSchema = new mongoose.Schema({
-    name: String,
-    img: String,
-    description: String
+    title: {
+        name: String,
+        img: String, 
+    },
+    top: {
+        name: String,
+        img: String,
+        description: String,
+        link: String, 
+    },
+    bottom: {
+        name: String,
+        img: String,
+        description: String,
+        link: String, 
+    },
+    shoe: {
+        name: String,
+        img: String,
+        description: String,
+        link: String, 
+    },
+    inspiration: {
+        name: String,
+        img: String,
+        description: String,
+    }
 })
 
 const Item = mongoose.model("Item", itemSchema)
