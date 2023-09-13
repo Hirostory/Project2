7d0a0c9bc614870cb356dfbf35a27cd1a63149f6
hirostory <hiroakisotooka@gmail.com>
2023-09-13 00:08:58 -0400

    all the starter layout of the up is working

---
+++ controller/stylemate.js
@@ -0,0 +1,33 @@
+const express = require('express') //connecting to express
+const router = express.Router()
+
+//conect my Schema to this file
+const Item = require('../models/stylemate.js')
+ 
+//the index route - GET
+router.get('/', async (req, res) => {
+    res.send('welcome to StyleMate')
+})
+
+//the new route - GET
+router.get('/new', (req, res) => {
+    res.send('New page is working')
+})
+
+//the show route - GET
+router.get('/:id', async (req, res) => {
+    res.send('show route is working')
+})
+
+//the edit route - GET
+router.post('/:id/edit', async (req, res) =>{
+    res.send('edit is working')
+})
+
+//the create route - POST
+
+//the update route - PUT
+
+//the delete route - DELETE
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // IMPORTS
 const express = require('express')
+const methodOverride = require("method-override")
 const app = express()
 
 require('dotenv').config()
@@ -19,9 +20,18 @@ db.on('error', (err) => { console.log('ERROR: ' , err)})
 db.on('connected', () => { console.log('mongo connected')})
 db.on('disconnected', () => { console.log('mongo disconnected')})
 
-app.get('/', (req, res) => {
-   res.send('Hello world!')
-})
+//importing it to my controller
+const styleController = require("./controller/stylemate.js")
+
+//MIDDLEWARE sections 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+app.use('/stylemate', styleController)
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//    res.send('Hello world!')
+// })
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`)
