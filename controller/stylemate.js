6ffeb36492f4fe7295375b0a44043183b30f8a36
hirostory <hiroakisotooka@gmail.com>
2023-09-14 18:37:17 -0400

    made the demo index and making sire the site is address is good

--- controller/stylemate.js
+++ controller/stylemate.js
@@ -9,7 +9,9 @@ router.get('/', async (req, res) => {
     // res.send('welcome to StyleMate')
     const foundItem = await Item.find()
     console.log(foundItem)
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        item: foundItem
+    })
 })
 
 //the new route - GET
@@ -33,6 +35,15 @@ router.get('/:id/edit', async (req, res) =>{
 })
 
 //the create route - POST
+router.post('/', async (req, res) => {
+    try{
+        const newItem = await Item.create(req.body)
+        res.redirect('/stylemate')
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err)
+    }
+})
 
 //the update route - PUT
 
